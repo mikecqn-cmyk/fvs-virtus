dc274555da345ce45f30ce715c99cc3063c63f7c
mikecqn-cmyk <mike.cqn@gmail.com>
2026-05-21 21:20:22 -0300

    Update cloud-config.js

--- cloud-config.js
+++ cloud-config.js
@@ -1,6 +1,6 @@
 window.FVS_CLOUD_CONFIG = {
   enabled: true,
-  supabaseUrl: "https://SEU-PROJETO.supabase.co",
-  supabaseAnonKey: "SUA-CHAVE-ANON",
+  supabaseUrl: "https://bdncfomzjaaykaymgagx.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkbmNmb216amFheWtheW1nYWd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0MDU3MTIsImV4cCI6MjA5NDk4MTcxMn0.gHSB4aOua1mrGHawe5UgilO5CisWSWwtgciwIcWjwf8",
   evidenceBucket: "fvs-evidencias",
 };
